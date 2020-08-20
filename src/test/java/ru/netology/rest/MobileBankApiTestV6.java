6526f513e764f77936a958c0c84dafca5eed9c5a
Vera Var <primavera1919@yandex.ru>
2020-08-21 02:12:12 +0300

    Tests modified

--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -7,6 +7,8 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.apache.commons.codec.digest.UnixCrypt.body;
 
 class MobileBankApiTestV6 {
     private RequestSpecification requestSpec = new RequestSpecBuilder()
@@ -29,6 +31,8 @@ class MobileBankApiTestV6 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
